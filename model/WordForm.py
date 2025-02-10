239c50d02d1f74a57194e842355d143326c9216a
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-10 12:21:32 +0300

    add field morphological_info

--- model/WordForm.py
+++ model/WordForm.py
@@ -1,8 +1,9 @@
 class WordForm:
-    def __init__(self, word_form, lemma, count):
+    def __init__(self, word_form, lemma, count, morphological_info=""):
         self.word_form = word_form
         self.lemma = lemma
         self.count = count
+        self.morphological_info = morphological_info
 
     def __repr__(self):
         return f"WordForm(word_form='{self.word_form}', lemma='{self.lemma}', count={self.count})"
