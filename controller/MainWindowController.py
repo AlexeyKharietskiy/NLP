6b7512f90942cb9fc64ea1d254ec7afd81f428bf
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-11 17:11:24 +0300

    add search function

--- controller/MainWindowController.py
+++ controller/MainWindowController.py
@@ -4,7 +4,8 @@ from model.TextConverter import TextConverter
 from logger import logger
 from model.TextProcessor import TextProcessor
 
-
+#TODO сохранение морфологической информации
+#TODO реализация поиска
 class MainWindowController:
     def __init__(self):
         self.window = None
@@ -31,15 +32,25 @@ class MainWindowController:
             text_processor = TextProcessor(text)
             self.word_form_list = text_processor.process_text()
             logger.info("Text has been processed")
-            return self.fill_in_table()
+            return self.fill_in_table(self.word_form_list)
         except Exception as e:
             #файлик оказался хуйней :(
             logger.error(f"File opening error: {e}")
 
-    def fill_in_table(self):
-        sorted_word_forms = sorted(self.word_form_list, key=lambda wf: wf.word_form.lower())
+    @staticmethod
+    def fill_in_table(word_list):
+        sorted_word_forms = sorted(word_list, key=lambda wf: wf.word_form.lower())
         table_data = [
             (wf.word_form, wf.lemma, wf.count, wf.morphological_info)  # Пустая строка для морфологической информации
             for wf in sorted_word_forms
         ]
         return table_data
+
+    def search_words(self, text):
+        query = text.strip().lower()
+        if not query:
+            return self.fill_in_table(self.word_form_list)
+
+        filtered_data = [row for row in self.word_form_list if query in str(row.word_form).lower()]
+        return self.fill_in_table(filtered_data)
+
--- view/MainView.py
+++ view/MainView.py
@@ -3,7 +3,7 @@ from tkinter import ttk, filedialog
 
 # следующий класс просто вставочка с прошлой лабы для раскрутки
 # пока просто не обращай внимания
-
+#TODO графа поиска
 class MainView(tk.Tk):
     def __init__(self, controller):
         super().__init__()
@@ -12,25 +12,35 @@ class MainView(tk.Tk):
         self.title('Лексический словарь')
         self.vocab_table = None
         self.menu = None
+        self.search_entry = None  # Поле для ввода поискового запроса
+        self.search_button = None  # Кнопка для запуска поиска
+        self.full_data = []  # Храним все данные для восстановления после поиска
 
     def create_view(self):
         # table
         columns = ["Wordform", "Lemma", "Occurance Frequency", "Morfological Info"]
         self.vocab_table = ttk.Treeview(self, columns=columns, show="headings")
-        self.vocab_table.grid(row=0, column=0, padx=[10, 10], pady=10, columnspan=3)
+        self.vocab_table.grid(row=1, column=0, padx=[10, 10], pady=10, columnspan=3)
         self.vocab_table.configure(height=30)
         self.vocab_table.heading("Wordform", text="Словоформа")
         self.vocab_table.heading("Lemma", text="Лемма")
         self.vocab_table.heading("Occurance Frequency", text="Частота появления")
         self.vocab_table.heading("Morfological Info", text="Морфологическая Информация")
-        scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.vocab_table.yview)
-        scrollbar.grid(row=0, column=2, sticky='nse')
 
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.vocab_table.yview)
+        scrollbar.grid(row=1, column=2, sticky='nse')
         self.vocab_table.configure(yscrollcommand=scrollbar.set)
 
         self.menu = tk.Menu(self, bg='lavender blush', fg='black', activebackground='lavender', activeforeground='black')
         self.menu.add_command(label="Открыть файл", command=self.open_file)
         self.config(menu=self.menu)
+
+        search_label = tk.Label(self, text="Поиск:")
+        search_label.grid(row=0, column=0, padx=10, pady=5, sticky='e')
+        self.search_entry = tk.Entry(self, width=30)
+        self.search_entry.grid(row=0, column=1, padx=5, pady=5, sticky='w')
+        self.search_button = tk.Button(self, text="Найти", command = self.perform_search)
+        self.search_button.grid(row=0, column=2, padx=5, pady=5)
 
     def main(self):
         self.create_view()
@@ -50,3 +60,6 @@ class MainView(tk.Tk):
         data = self.controller.open_file(filename)
         self.populate_table(data)
 
+    def perform_search(self):
+        searched_word_list = self.controller.search_words(self.search_entry.get())
+        self.populate_table(searched_word_list)
