dc915b472630c036d5e36214cd0b5cbf0d0c2f8f
Alexey Kharietskiy <slashertoor@gmail.com>
2025-04-03 00:43:01 +0300

    change text processor logic

--- src/core/text/text_processor.py
+++ src/core/text/text_processor.py
@@ -1,40 +1,62 @@
+import enum
 from natasha import Doc, Segmenter, MorphVocab, NewsEmbedding, NewsMorphTagger
 
+from schemas.words import WordSchema
+
+class WordPos(enum.Enum):
+    NOUN = "имя существительное"
+    ADJ = "имя прилагательное"
+    VERB = "глагол"
+    INFN = "инфинитив"
+    PRTF = "причастие"
+    PRTS = "деепричастие"
+    NUMR = "числительное"
+    ADV = "наречие"
+    PRON = "местоимение"
+    PRED = "предикатив"
+    ADP = "предлог"
+    SCONJ = "подчинительный союз"
+    CCONJ = "сочинительный союз"
+    PART = "частица"
+    INTJ = "междометие"
+    PROPN = "название"
+    DET = 'определитель'
 class TextProcessor:
     def __init__(self):
         self.segmenter = Segmenter()
         self.morph_vocab = MorphVocab()
         self.morph_tagger = NewsMorphTagger(NewsEmbedding())
         self.doc = Doc
-        
-    def process(self, text: str):
-        ...
-        
-    def get_sentences(self, text: str):
-        sentences = list(self.segmenter.analyze(text))
-        return sentences
     
-    def get_tokens(self, sentence: str):
-        '''Input data sentence from TextProcessor.get_sentences'''
-        lower_text = sentence.lower()
+    def get_tokens(self, text: str) -> list[WordSchema]:
+        '''Input data - text'''
+        lower_text = text.lower()
         self.doc = Doc(lower_text)
         self.doc.segment(self.segmenter)
         self.doc.tag_morph(self.morph_tagger)
         tokens = []
         for token in self.doc.tokens:
             if token.pos in {
-                "NOUN", "VERB", "ADJ", "NUM"
+                "NOUN", "VERB", "ADJ", "NUMR"
                 "ADV", "PRON", "PROPN", "PART", 
-                "CCONJ", "SCONJ", "ADP", "DET" 
+                "CCONJ", "SCONJ", "ADP", "DET",
+                "INFN", 'PRTF', 'PRTS', 'PRED',
+                'INTJ'
                 }:
                 token.lemmatize(self.morph_vocab)
-                curr_token = {
-                    'wordform':token.text,
-                    'lemma':token.lemma,
-                    'pos':token.pos,
-                    'feats':token.feats
-                    }
-                tokens.append(curr_token)
+                curr_token = WordSchema(
+                    word=token.text, 
+                    lemma= token.lemma, 
+                    part_of_speech= WordPos[token.pos],
+                    frequency= 1,
+                    feats= str(token.feats)
+                )
+                if curr_token not in tokens:
+                    tokens.append(curr_token)
+                else: 
+                    for i, tok in enumerate(tokens):
+                        if tok == curr_token:
+                            tokens[i].frequency+=1                            
         return tokens
     
     
--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
 import asyncio
 from typing import Annotated
-
 from sqlalchemy import String, create_engine, text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
@@ -9,7 +8,7 @@ from config import settings
 
 sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
-    # echo=True
+    echo=True
 )
 
 async_engine = create_async_engine(
@@ -21,4 +20,14 @@ sync_session_factory = sessionmaker(sync_engine)
 async_session_factory = async_sessionmaker(async_engine)
 
 class Base(DeclarativeBase):
-    ...
+    repr_cols_num = 3
+    repr_cols = tuple()
+    
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
+    
