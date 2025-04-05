c8b1373b0942c6fc0266f1aa7804a2571c36aad5
Alexey Kharietskiy <slashertoor@gmail.com>
2025-04-05 13:38:15 +0300

    create branch structure

--- lab2/src/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from api.words import router as words_router
-from api.concordances import router as concord_router
-from api.texts import router as texts_router
-
-main_router = APIRouter()
-main_router.include_router(words_router)
-main_router.include_router(texts_router)
-main_router.include_router(concord_router)
--- lab2/src/core/text/text_parsers/file_parser.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Parser(ABC):
-    @abstractmethod
-    def parse(self, file_path: str) -> str:
-        pass
