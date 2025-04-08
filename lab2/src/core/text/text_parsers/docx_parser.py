ddf69a17ae661e97adb3e7a72ef113f299ee6adc
Alexey Kharietskiy <slashertoor@gmail.com>
2025-04-08 03:04:35 +0300

    add \n  symbols processing

--- lab2/src/core/text/text_parsers/docx_parser.py
+++ lab2/src/core/text/text_parsers/docx_parser.py
@@ -6,4 +6,4 @@ from core.text.text_parsers.file_parser import Parser
 class DocxParser(Parser):
     def parse(self, file_path: str) -> str:
         doc = Document(file_path)
-        return "\n".join([paragraph.text for paragraph in doc.paragraphs])
+        return " ".join([paragraph.text for paragraph in doc.paragraphs])
--- lab2/src/core/text/text_parsers/txt_parser.py
+++ lab2/src/core/text/text_parsers/txt_parser.py
@@ -1,8 +1,8 @@
-
 from core.text.text_parsers.file_parser import Parser
 
 
 class TxtParser(Parser):
     def parse(self, file_path: str) -> str:
         with open(file_path, "r", encoding="utf-8") as file:
-            return file.read()
+            text = file.read()
+            return text.replace("\n", " ")
