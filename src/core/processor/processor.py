95d65f260bf93d4e482de3d5a7b7141f6260562e
Alexey Kharietskiy <slashertoor@gmail.com>
2025-05-01 18:49:56 +0300

    add text processor

---
+++ src/core/processor/processor.py
@@ -0,0 +1,80 @@
+from natasha import (
+    MorphVocab,
+    Segmenter, 
+    NewsEmbedding, 
+    NewsSyntaxParser, 
+    Doc,
+    NewsMorphTagger,
+    NewsNERTagger
+    )
+
+
+class TextProcessor:
+    def __init__(self):
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        self.embedding = NewsEmbedding()
+        self.morph_tagger = NewsMorphTagger(NewsEmbedding())
+        self.syntax_parser = NewsSyntaxParser(self.embedding)
+        self.ner_tagger = NewsNERTagger(self.embedding)
+        
+    def process(self, text: str) -> list[str]:
+        doc = Doc(text)
+        doc.segment(self.segmenter)
+
+        result = {
+            'text': text,
+            'sentences': [],
+        }
+        for sentence in doc.sents:
+            current_sentence = {}
+            current_sentence['sentence'] = sentence.text
+            words = self.get_words(sentence.text)
+            ners = self.get_ners(sentence.text)
+            current_sentence['words'] = words
+            current_sentence['ners'] = ners
+            result['sentences'].append(current_sentence)
+        return result
+
+    def get_words(self, sentence: str) -> list[dict]:
+        doc = Doc(sentence)
+        doc.segment(self.segmenter)
+        doc.parse_syntax(self.syntax_parser)
+        doc.tag_morph(self.morph_tagger)
+        
+        result = []
+        
+        for token in doc.tokens:
+            if hasattr(token, 'rel'):
+                head_word = next(
+                    (t.text for t in doc.tokens if t.id == token.head_id),
+                    ''
+                )
+                
+                result.append(
+                    {
+                        'word': token.text,
+                        'pos': token.pos,
+                        'head_word': head_word,
+                        'relation': token.rel,
+                        'feats': str(token.feats)
+                    }
+                )
+        return result
+    
+    def get_ners(self, sentence: str) -> list[dict]:
+        doc = Doc(sentence)
+        doc.segment(self.segmenter)
+        doc.parse_syntax(self.syntax_parser)
+        doc.tag_morph(self.morph_tagger)
+        doc.tag_ner(self.ner_tagger)
+
+        result = []
+
+        for span in doc.spans:
+            result.append({
+                'text': span.text,
+                'type': span.type,
+            })
+
+        return result
